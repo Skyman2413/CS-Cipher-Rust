1555f1a3655e2c1a7bcf120b2828df28c1796ea0
Stepan <stepan26062001@gmail.com>
2025-01-10 14:01:55 +0300

    add command-matching

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use crate::cli::Cli;
+use crate::cli::{Cli, Commands};
 use clap::Parser;
+use crate::cipher::Cipher;
 
 mod cli;
+mod cipher;
+mod errors;
 
 fn main() {
     let cli = Cli::parse();
+    match &cli.command {
+        Commands::Encrypt(encrypt_args) => {
+            Cipher::build(
+                &encrypt_args.input,
+                true,
+                &encrypt_args.key_path,
+                &encrypt_args.output,
+            ).unwrap();
+        }
+        Commands::Decrypt(decrypt_args) => {}
+    }
 }
